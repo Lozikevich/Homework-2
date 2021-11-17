0ac5293c9e6d08811e631a025943f7a8eac56bf4
Lozikevich <94119373+Lozikevich@users.noreply.github.com>
2021-11-17 15:48:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,31 +10,24 @@ if __name__ == '__main__':
         for p in range(len(device.data)):
             a = read_line(device)
             res.insert(p, a)
-
         device: Device = open_device('/devices/dev3')
-        i = len(device.data)
-
         for k in range(len(res)):
-            write_line(device, res[k], i)
-            i = i + 1
+            write_line(device, res[k])
         print(device.data)
-
     elif ch1 == 'r':
         ch2 = 'y'
         device: Device = open_device('/devices/dev3')
-        i = len(device.data)
         while ch2 == 'y':
             print('Введите строку для записи в устройство:')
             a = input()
             try:
-                i = i + 1
-                write_line(device, a, i)
+                # i = i + 1
+                write_line(device, a)
                 print(device.data)
                 print('Хотите ввести еще строку для записи в устройство?')
                 ch2 = input()
                 if ch2 == 'n':
-                    print('Buy!')
+                    print('Bye!')
                     break
-
             except Exception as exception:
                 print(f'Error: {exception}')
